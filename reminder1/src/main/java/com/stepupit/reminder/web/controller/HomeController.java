fb24b7e59e47e76d7bd357ded6e5e4d9da235d41
ankur <ankurnehatogether@gmail.com>
2014-05-10 16:31:10 +0530

    comments added

--- reminder1/src/main/java/com/stepupit/reminder/web/controller/HomeController.java
+++ reminder1/src/main/java/com/stepupit/reminder/web/controller/HomeController.java
@@ -1,7 +1,5 @@
 package com.stepupit.reminder.web.controller;
 
-
-
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -14,14 +12,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/welcome.htm")
 public class HomeController {
-	
+
 	private static final Logger logger = Logger.getLogger(HomeController.class);
-	
+
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
- 
 		model.addAttribute("message", "Spring 3 MVC Hello World");
 		return "newContact";
 	}
-	
+
 }
