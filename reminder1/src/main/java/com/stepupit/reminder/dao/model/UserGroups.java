4e939fb8174d33c1e8a61c01df24a72145494abe
ankur <ankurnehatogether@gmail.com>
2014-05-10 15:19:30 +0530

    renamed

--- reminder1/src/main/java/com/stepupit/reminder/dao/model/UserGroups.java
+++ reminder1/src/main/java/com/stepupit/reminder/dao/model/UserGroups.java
@@ -1,11 +1,13 @@
-/*package com.stepupit.reminder.dao.model;
+package com.stepupit.reminder.dao.model;
 
 
- * To change this template, choose Tools | Templates
+/* To change this template, choose Tools | Templates
  * and open the template in the editor.
+ * */
 
 
 import java.io.Serializable;
+
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -15,18 +17,18 @@ import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import javax.xml.bind.annotation.XmlRootElement;
 
- *//**
+/**
  *
  * @author ank
- *//*
+ */
 @Entity
 @Table(name = "usergroups")
 @XmlRootElement
 @NamedQueries({
-		@NamedQuery(name = "Usergroups.findAll", query = "SELECT u FROM Usergroups u"),
-		@NamedQuery(name = "Usergroups.findByGrpId", query = "SELECT u FROM Usergroups u WHERE u.grpId = :grpId"),
-		@NamedQuery(name = "Usergroups.findByGrpName", query = "SELECT u FROM Usergroups u WHERE u.grpName = :grpName"),
-		@NamedQuery(name = "Usergroups.findByUserId", query = "SELECT u FROM Usergroups u WHERE u.userId = :userId") })
+	@NamedQuery(name = "Usergroups.findAll", query = "SELECT u FROM Usergroups u"),
+	@NamedQuery(name = "Usergroups.findByGrpId", query = "SELECT u FROM Usergroups u WHERE u.grpId = :grpId"),
+	@NamedQuery(name = "Usergroups.findByGrpName", query = "SELECT u FROM Usergroups u WHERE u.grpName = :grpName"),
+	@NamedQuery(name = "Usergroups.findByUserId", query = "SELECT u FROM Usergroups u WHERE u.userId = :userId") })
 public class UserGroups implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
@@ -45,37 +47,6 @@ public class UserGroups implements Serializable {
 		this.grpId = grpId;
 	}
 
-	public Integer getGrpId() {
-		return grpId;
-	}
-
-	public void setGrpId(Integer grpId) {
-		this.grpId = grpId;
-	}
-
-	public String getGrpName() {
-		return grpName;
-	}
-
-	public void setGrpName(String grpName) {
-		this.grpName = grpName;
-	}
-
-	public Integer getUserId() {
-		return userId;
-	}
-
-	public void setUserId(Integer userId) {
-		this.userId = userId;
-	}
-
-	@Override
-	public int hashCode() {
-		int hash = 0;
-		hash += (grpId != null ? grpId.hashCode() : 0);
-		return hash;
-	}
-
 	@Override
 	public boolean equals(Object object) {
 		// TODO: Warning - this method won't work in the case the id fields are
@@ -91,10 +62,40 @@ public class UserGroups implements Serializable {
 		return true;
 	}
 
+	public Integer getGrpId() {
+		return this.grpId;
+	}
+
+	public String getGrpName() {
+		return this.grpName;
+	}
+
+	public Integer getUserId() {
+		return this.userId;
+	}
+
+	@Override
+	public int hashCode() {
+		int hash = 0;
+		hash += (this.grpId != null ? this.grpId.hashCode() : 0);
+		return hash;
+	}
+
+	public void setGrpId(Integer grpId) {
+		this.grpId = grpId;
+	}
+
+	public void setGrpName(String grpName) {
+		this.grpName = grpName;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
 	@Override
 	public String toString() {
-		return "javaapplication2.Usergroups[ grpId=" + grpId + " ]";
+		return "javaapplication2.Usergroups[ grpId=" + this.grpId + " ]";
 	}
 
 }
-  */
