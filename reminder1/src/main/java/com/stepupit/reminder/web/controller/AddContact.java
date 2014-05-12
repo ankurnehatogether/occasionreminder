87afed3e66b03ef594c0205e5996b0389443dd07
ankur <ankurnehatogether@gmail.com>
2014-05-12 18:58:19 +0530

    Added DAO/WEB model application service layer and registration page

--- reminder1/src/main/java/com/stepupit/reminder/web/controller/AddContact.java
+++ reminder1/src/main/java/com/stepupit/reminder/web/controller/AddContact.java
@@ -1,21 +1,11 @@
 package com.stepupit.reminder.web.controller;
 
-import org.apache.log4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
-
-import com.stepupit.reminder.dao.model.UserGroups;
-import com.stepupit.reminder.dao.service.UserGroupService;
 
 @Controller
 public class AddContact {
 
-	@Autowired
+	/*@Autowired
 	@Qualifier("UserGroupService")
 	private UserGroupService userGroupService;
 	private static final Logger logger = Logger.getLogger(AddContact.class);
@@ -35,6 +25,6 @@ public class AddContact {
 		this.userGroupService.addGroup(group);
 		mav.addObject("message", group.getGrpName());
 		return mav;
-	}
+	}*/
 
 }
