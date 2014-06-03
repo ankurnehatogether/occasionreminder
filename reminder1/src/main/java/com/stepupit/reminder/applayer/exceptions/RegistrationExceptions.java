a8d797e5c1af9e6fb3472103669fdde8b675e868
ankur <ankurnehatogether@gmail.com>
2014-06-03 21:56:59 +0530

    added application layer exception pkg

---
+++ reminder1/src/main/java/com/stepupit/reminder/applayer/exceptions/RegistrationExceptions.java
@@ -0,0 +1,28 @@
+package com.stepupit.reminder.applayer.exceptions;
+
+/**
+ *
+ * @author ank
+ */
+
+public class RegistrationExceptions extends Exception {
+
+	private static final long serialVersionUID = -8515034456417992020L;
+
+	public RegistrationExceptions() {
+		super();
+	}
+
+	public RegistrationExceptions(String message) {
+		super(message);
+	}
+
+	public RegistrationExceptions(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public RegistrationExceptions(Throwable cause) {
+		super(cause);
+	}
+
+}
